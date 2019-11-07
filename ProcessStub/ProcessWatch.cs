63de107918866110bb768140117728f7554ebd8a
NarryG <me@narry.land>
2019-11-06 22:06:39 -0500

    Version bump for recompile for hex editor

--- ProcessStub/ProcessWatch.cs
+++ ProcessStub/ProcessWatch.cs
@@ -16,7 +16,7 @@ namespace ProcessStub
 {
     public static class ProcessWatch
     {
-        public static string ProcessStubVersion = "0.0.7";
+        public static string ProcessStubVersion = "0.0.8";
         public static string currentDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
         public static Process p;
         public static bool UseFiltering = true;
